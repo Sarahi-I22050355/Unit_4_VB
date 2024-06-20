5ebde215f624ddcf673b104f966fab7ad2302cd4
Sarah <Sarah@HP-SARAHI>
2024-06-20 05:18:58 -0600

    Unit 4 VB

---
+++ Form1.Designer.vb
@@ -0,0 +1,241 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim ChartArea7 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend7 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series7 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnSaveNewFile = New System.Windows.Forms.Button()
+        Me.label2 = New System.Windows.Forms.Label()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.txtSearchCSV = New System.Windows.Forms.TextBox()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnOpen = New System.Windows.Forms.Button()
+        Me.dgvTableCSV = New System.Windows.Forms.DataGridView()
+        Me.Product = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Revenue = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        CType(Me.dgvTableCSV, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.btnUpdate.Font = New System.Drawing.Font("Arial Narrow", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnUpdate.Location = New System.Drawing.Point(854, 362)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(149, 43)
+        Me.btnUpdate.TabIndex = 20
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnSaveNewFile
+        '
+        Me.btnSaveNewFile.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.btnSaveNewFile.Font = New System.Drawing.Font("Arial Narrow", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSaveNewFile.Location = New System.Drawing.Point(181, 356)
+        Me.btnSaveNewFile.Name = "btnSaveNewFile"
+        Me.btnSaveNewFile.Size = New System.Drawing.Size(149, 43)
+        Me.btnSaveNewFile.TabIndex = 19
+        Me.btnSaveNewFile.Text = "Save new file"
+        Me.btnSaveNewFile.UseVisualStyleBackColor = True
+        '
+        'label2
+        '
+        Me.label2.AutoSize = True
+        Me.label2.Font = New System.Drawing.Font("Arial Narrow", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label2.Location = New System.Drawing.Point(532, 151)
+        Me.label2.Name = "label2"
+        Me.label2.Size = New System.Drawing.Size(102, 22)
+        Me.label2.TabIndex = 18
+        Me.label2.Text = "Text to Search"
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Font = New System.Drawing.Font("Arial Narrow", 22.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label1.Location = New System.Drawing.Point(18, 35)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(271, 43)
+        Me.label1.TabIndex = 17
+        Me.label1.Text = "Inventory Manager"
+        '
+        'txtSearchCSV
+        '
+        Me.txtSearchCSV.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtSearchCSV.Font = New System.Drawing.Font("Arial Narrow", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSearchCSV.Location = New System.Drawing.Point(536, 176)
+        Me.txtSearchCSV.Name = "txtSearchCSV"
+        Me.txtSearchCSV.Size = New System.Drawing.Size(181, 28)
+        Me.txtSearchCSV.TabIndex = 16
+        '
+        'btnDelete
+        '
+        Me.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.btnDelete.Font = New System.Drawing.Font("Arial Narrow", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.Location = New System.Drawing.Point(536, 259)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(149, 43)
+        Me.btnDelete.TabIndex = 15
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnSearch
+        '
+        Me.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.btnSearch.Font = New System.Drawing.Font("Arial Narrow", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSearch.Location = New System.Drawing.Point(536, 210)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(149, 43)
+        Me.btnSearch.TabIndex = 14
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.btnSave.Font = New System.Drawing.Font("Arial Narrow", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSave.Location = New System.Drawing.Point(181, 307)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(149, 43)
+        Me.btnSave.TabIndex = 13
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnOpen
+        '
+        Me.btnOpen.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.btnOpen.Font = New System.Drawing.Font("Arial Narrow", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOpen.Location = New System.Drawing.Point(26, 307)
+        Me.btnOpen.Name = "btnOpen"
+        Me.btnOpen.Size = New System.Drawing.Size(149, 43)
+        Me.btnOpen.TabIndex = 12
+        Me.btnOpen.Text = "Open"
+        Me.btnOpen.UseVisualStyleBackColor = True
+        '
+        'dgvTableCSV
+        '
+        Me.dgvTableCSV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvTableCSV.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Product, Me.Column3, Me.Column4, Me.Revenue})
+        Me.dgvTableCSV.Location = New System.Drawing.Point(26, 81)
+        Me.dgvTableCSV.MultiSelect = False
+        Me.dgvTableCSV.Name = "dgvTableCSV"
+        Me.dgvTableCSV.RowHeadersVisible = False
+        Me.dgvTableCSV.RowHeadersWidth = 51
+        Me.dgvTableCSV.RowTemplate.Height = 24
+        Me.dgvTableCSV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvTableCSV.Size = New System.Drawing.Size(504, 220)
+        Me.dgvTableCSV.TabIndex = 11
+        '
+        'Product
+        '
+        Me.Product.HeaderText = "Product"
+        Me.Product.MinimumWidth = 6
+        Me.Product.Name = "Product"
+        Me.Product.Width = 125
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Quantity"
+        Me.Column3.MinimumWidth = 6
+        Me.Column3.Name = "Column3"
+        Me.Column3.Width = 125
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "Price"
+        Me.Column4.MinimumWidth = 6
+        Me.Column4.Name = "Column4"
+        Me.Column4.Width = 125
+        '
+        'Revenue
+        '
+        Me.Revenue.HeaderText = "Revenue"
+        Me.Revenue.MinimumWidth = 6
+        Me.Revenue.Name = "Revenue"
+        Me.Revenue.Width = 125
+        '
+        'Chart1
+        '
+        ChartArea7.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea7)
+        Legend7.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend7)
+        Me.Chart1.Location = New System.Drawing.Point(852, 33)
+        Me.Chart1.Name = "Chart1"
+        Series7.ChartArea = "ChartArea1"
+        Series7.Legend = "Legend1"
+        Series7.Name = "Series1"
+        Me.Chart1.Series.Add(Series7)
+        Me.Chart1.Size = New System.Drawing.Size(517, 316)
+        Me.Chart1.TabIndex = 21
+        Me.Chart1.Text = "Chart1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.ClientSize = New System.Drawing.Size(1470, 440)
+        Me.Controls.Add(Me.Chart1)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnSaveNewFile)
+        Me.Controls.Add(Me.label2)
+        Me.Controls.Add(Me.label1)
+        Me.Controls.Add(Me.txtSearchCSV)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnOpen)
+        Me.Controls.Add(Me.dgvTableCSV)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvTableCSV, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents btnUpdate As Button
+    Private WithEvents btnSaveNewFile As Button
+    Private WithEvents label2 As Label
+    Private WithEvents label1 As Label
+    Private WithEvents txtSearchCSV As TextBox
+    Private WithEvents btnDelete As Button
+    Private WithEvents btnSearch As Button
+    Private WithEvents btnSave As Button
+    Private WithEvents btnOpen As Button
+    Private WithEvents dgvTableCSV As DataGridView
+    Private WithEvents Product As DataGridViewTextBoxColumn
+    Private WithEvents Column3 As DataGridViewTextBoxColumn
+    Private WithEvents Column4 As DataGridViewTextBoxColumn
+    Private WithEvents Revenue As DataGridViewTextBoxColumn
+    Friend WithEvents Chart1 As DataVisualization.Charting.Chart
+End Class
---
+++ Form1.vb
@@ -0,0 +1,328 @@
+﻿Imports System.IO
+Imports System.Text
+Imports System.Windows.Forms.DataVisualization.Charting
+
+Public Class Form1
+    Private filePathCSV As String
+
+    Private Sub btnOpen_Click(sender As Object, e As EventArgs) Handles btnOpen.Click
+        Dim openFileDialog As New OpenFileDialog()
+        openFileDialog.Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*"
+
+        If openFileDialog.ShowDialog() = DialogResult.Cancel Then
+            Return
+        End If
+
+        filePathCSV = openFileDialog.FileName
+        ShowCSV_InDGV()
+
+    End Sub
+
+    Public Sub ShowCSV_InDGV()
+        dgvTableCSV.Rows.Clear()
+        dgvTableCSV.Columns.Clear()
+
+        ' Leer líneas del archivo CSV
+        Dim lines As String() = File.ReadAllLines(filePathCSV)
+
+        ' Si hay líneas en el archivo
+        If lines.Length > 0 Then
+            ' Obtener los nombres de las columnas del primer registro
+            Dim columnNames As String() = lines(0).Split(","c)
+
+            ' Agregar columnas al DataGridView usando los nombres de las columnas del CSV
+            For Each columnName As String In columnNames
+                dgvTableCSV.Columns.Add(columnName, columnName)
+            Next
+
+            ' Agregar filas al DataGridView con el contenido del CSV (excluyendo la primera línea)
+            For i As Integer = 1 To lines.Length - 1
+                Dim fields As String() = lines(i).Split(","c)
+                dgvTableCSV.Rows.Add(fields)
+            Next
+        End If
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        If String.IsNullOrEmpty(filePathCSV) OrElse Not File.Exists(filePathCSV) Then
+            MessageBox.Show("Select a file to work with.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Return
+        End If
+
+        If txtSearchCSV.Text = "" Then
+            MessageBox.Show("To search, you must use a NAME.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Return
+        End If
+
+        Try
+            ' Leer el archivo CSV línea por línea
+            Using reader As New StreamReader(filePathCSV)
+                While Not reader.EndOfStream
+                    Dim line As String = reader.ReadLine()
+                    Dim fields As String() = line.Split(","c)
+
+                    ' Comparar el término de búsqueda con el primer campo (en este caso, el nombre)
+                    If fields.Length > 0 AndAlso fields(0) = txtSearchCSV.Text Then
+                        MessageBox.Show("Found: " & String.Join(", ", fields), "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                        Return
+                    End If
+                End While
+            End Using
+        Catch ex As Exception
+            Console.WriteLine("Error searching in the CSV file: " & ex.Message)
+        End Try
+
+        MessageBox.Show("No matching name found in the file", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        ' Verificar si hay datos en el DataGridView
+        If dgvTableCSV.Rows.Count = 0 Then
+            MessageBox.Show("No hay datos para guardar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        Try
+            ' Obtener la ruta del archivo CSV
+            Dim filePath As String = filePathCSV
+
+            ' Crear un StringBuilder para construir el contenido del archivo CSV
+            Dim csvContent As New StringBuilder()
+
+            ' Agregar los encabezados de columna al archivo CSV
+            For i As Integer = 0 To dgvTableCSV.Columns.Count - 1
+                csvContent.Append(dgvTableCSV.Columns(i).HeaderText)
+                If i < dgvTableCSV.Columns.Count - 1 Then
+                    csvContent.Append(",")
+                End If
+            Next
+            csvContent.AppendLine() ' Agregar nueva línea después de los encabezados
+
+            ' Obtener los datos y los índices de las filas que tienen valores en la columna "Revenue"
+            Dim revenueData = dgvTableCSV.Rows.Cast(Of DataGridViewRow)() _
+                        .Where(Function(row) row.Cells("Revenue").Value IsNot Nothing) _
+                        .Select(Function(row) New With {.Value = Convert.ToDecimal(row.Cells("Revenue").Value), .Index = row.Index}) _
+                        .OrderByDescending(Function(row) row.Value) _
+                        .ToList()
+
+            ' Agregar datos de cada fila al archivo CSV en el orden ordenado
+            For Each rowData In revenueData
+                Dim row As DataGridViewRow = dgvTableCSV.Rows(rowData.Index)
+                Dim hasData As Boolean = False ' Bandera para verificar si la fila tiene celdas no vacías
+                Dim rowDataStr As New StringBuilder()
+
+                For i As Integer = 0 To dgvTableCSV.Columns.Count - 1
+                    ' Verificar si el valor de la celda no es nulo ni está vacío
+                    If row.Cells(i).Value IsNot Nothing AndAlso Not String.IsNullOrWhiteSpace(row.Cells(i).Value.ToString()) Then
+                        rowDataStr.Append(row.Cells(i).Value.ToString())
+                        hasData = True ' Establecer la bandera en true si la celda tiene un valor no vacío
+                    End If
+
+                    If i < dgvTableCSV.Columns.Count - 1 Then
+                        rowDataStr.Append(",")
+                    End If
+                Next
+
+                ' Agregar los datos de la fila al contenido del CSV si la fila tiene celdas no vacías
+                If hasData Then
+                    csvContent.AppendLine(rowDataStr.ToString())
+                End If
+            Next
+
+            ' Escribir el contenido en el archivo CSV
+            File.WriteAllText(filePath, csvContent.ToString())
+
+            MessageBox.Show("Los datos se guardaron correctamente en el archivo CSV.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            MessageBox.Show("Error al guardar datos en el archivo CSV: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+        ShowCSV_InDGV()
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        ' Check if a file is selected
+        If String.IsNullOrWhiteSpace(filePathCSV) OrElse Not File.Exists(filePathCSV) Then
+            MessageBox.Show("Select a valid file to work with.", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Return
+        End If
+
+        ' Check if a name to search for is entered
+        If String.IsNullOrWhiteSpace(txtSearchCSV.Text) Then
+            MessageBox.Show("Please enter a name to search for.", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Return
+        End If
+
+        Try
+            ' Read the CSV file line by line and write the non-deleted lines to a new temporary file
+            Dim tempFilePath As String = Path.GetTempFileName()
+            Using reader As New StreamReader(filePathCSV)
+                Using writer As New StreamWriter(tempFilePath)
+                    Dim line As String = reader.ReadLine()
+                    While line IsNot Nothing
+                        ' Check if the current line contains the name to delete
+                        If Not line.Contains(txtSearchCSV.Text) Then
+                            writer.WriteLine(line)
+                        End If
+                        line = reader.ReadLine()
+                    End While
+                End Using
+            End Using
+
+            ' Replace the original file with the temporary file
+            File.Delete(filePathCSV)
+            File.Move(tempFilePath, filePathCSV)
+            ShowCSV_InDGV()
+        Catch ex As Exception
+            MessageBox.Show("Error deleting line from the CSV file: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+    End Sub
+
+    Private Sub btnSaveNewFile_Click(sender As Object, e As EventArgs) Handles btnSaveNewFile.Click
+        ' Check if there is data in the DataGridView
+        If dgvTableCSV.Rows.Count = 0 Then
+            MessageBox.Show("There is no data to save.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        ' Get the index of the "Revenue" column
+        Dim revenueColumnIndex As Integer = dgvTableCSV.Columns("Revenue").Index
+
+        ' Obtain data and row indices of the "Revenue" column, excluding the last row
+        Dim revenueData = dgvTableCSV.Rows.Cast(Of DataGridViewRow)() _
+    .Where(Function(row) row.Index <> dgvTableCSV.Rows.Count - 1 AndAlso row.Cells(revenueColumnIndex).Value IsNot Nothing) _
+    .Select(Function(row) Convert.ToDecimal(row.Cells(revenueColumnIndex).Value)) _
+    .ToList()
+
+        Dim rowIndices = dgvTableCSV.Rows.Cast(Of DataGridViewRow)() _
+    .Where(Function(row) row.Index <> dgvTableCSV.Rows.Count - 1 AndAlso row.Cells(revenueColumnIndex).Value IsNot Nothing) _
+    .Select(Function(row) row.Index) _
+    .ToList()
+
+        ' Sort the revenue data and row indices in descending order
+        Dim sortedData = revenueData.OrderByDescending(Function(x) x).ToList()
+        Dim sortedIndices = rowIndices.OrderByDescending(Function(x) revenueData(rowIndices.IndexOf(x))).ToList()
+
+        ' Create a SaveFileDialog instance
+        Dim saveFileDialog As New SaveFileDialog()
+        saveFileDialog.Filter = "CSV Files (*.csv)|*.csv"
+        saveFileDialog.Title = "Save as CSV File"
+
+        ' Set initial directory and default file name
+        saveFileDialog.InitialDirectory = Path.GetDirectoryName(filePathCSV)
+        saveFileDialog.FileName = Path.GetFileNameWithoutExtension(filePathCSV) & "_new.csv"
+
+        If saveFileDialog.ShowDialog() = DialogResult.Cancel Then
+            Return
+        End If
+
+        filePathCSV = saveFileDialog.FileName
+
+        Try
+            ' Create a StringBuilder to build the content of the CSV file
+            Dim csvContent As New StringBuilder()
+
+            ' Add column headers to the CSV file
+            For i As Integer = 0 To dgvTableCSV.Columns.Count - 1
+                csvContent.Append(dgvTableCSV.Columns(i).HeaderText)
+                If i < dgvTableCSV.Columns.Count - 1 Then
+                    csvContent.Append(",")
+                End If
+            Next
+            csvContent.AppendLine() ' Add new line after headers
+
+            ' Add data from each row to the CSV file
+            For Each rowIndex As Integer In sortedIndices
+                Dim row As DataGridViewRow = dgvTableCSV.Rows(rowIndex)
+                Dim hasData As Boolean = False ' Flag to check if the row has any non-empty cells
+                Dim rowData As New StringBuilder()
+
+                For i As Integer = 0 To dgvTableCSV.Columns.Count - 1
+                    ' Check if the cell value is not null or empty
+                    If row.Cells(i).Value IsNot Nothing AndAlso Not String.IsNullOrWhiteSpace(row.Cells(i).Value.ToString()) Then
+                        rowData.Append(row.Cells(i).Value.ToString())
+                        hasData = True ' Set flag to true if the cell has non-empty value
+                    End If
+
+                    If i < dgvTableCSV.Columns.Count - 1 Then
+                        rowData.Append(",")
+                    End If
+                Next
+
+                ' Add row data to the CSV content if the row has any non-empty cells
+                If hasData Then
+                    csvContent.AppendLine(rowData.ToString())
+                End If
+            Next
+
+            ' Write the content to the CSV file
+            File.WriteAllText(filePathCSV, csvContent.ToString())
+
+            MessageBox.Show("Data saved successfully to the CSV file.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            MessageBox.Show("Error saving data to the CSV file: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+        ShowCSV_InDGV()
+
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        ' Obtener los datos del DataGridView
+        Dim data As New Dictionary(Of String, Decimal)()
+        For Each row As DataGridViewRow In dgvTableCSV.Rows
+            ' Verificar si la celda "Product" no es nula
+            If row.Cells("Product").Value IsNot Nothing Then
+                Dim product As String = row.Cells("Product").Value.ToString()
+                Dim revenue As Decimal = 0
+
+                ' Verificar si la celda "Revenue" no es nula
+                If row.Cells("Revenue").Value IsNot Nothing AndAlso Decimal.TryParse(row.Cells("Revenue").Value.ToString(), revenue) Then
+                    If Not data.ContainsKey(product) Then
+                        data.Add(product, revenue)
+                    End If
+                End If
+            End If
+        Next
+
+        ' Limpiar el Chart antes de agregar nuevos datos
+        Chart1.Series.Clear()
+        Chart1.ChartAreas(0).AxisX.Title = "Productos"
+        Chart1.ChartAreas(0).AxisY.Title = "Revenue"
+
+        ' Definir una lista de colores diferentes para cada serie
+        Dim colors As Color() = {Color.Red, Color.Blue, Color.Green, Color.Orange, Color.Purple, Color.Yellow}
+
+        ' Contador para iterar sobre la lista de colores
+        Dim colorIndex As Integer = 0
+
+        ' Agregar datos al Chart
+        For Each entry In data
+            Dim product As String = entry.Key
+            Dim revenue As Decimal = entry.Value
+
+            ' Agregar una nueva serie al gráfico con el nombre del producto
+            Chart1.Series.Add(product)
+
+            ' Asignar un color diferente a la serie actual
+            Chart1.Series(product).Color = colors(colorIndex)
+
+            ' Agregar el punto (valor de revenue) a la serie actual
+            Chart1.Series(product).Points.AddY(revenue)
+
+            ' Incrementar el índice del color
+            colorIndex += 1
+
+            ' Reiniciar el índice del color si se excede el número de colores definidos
+            If colorIndex >= colors.Length Then
+                colorIndex = 0
+            End If
+        Next
+
+        ' Personalizar el gráfico (opcional)
+        Chart1.ChartAreas(0).AxisX.Interval = 1 ' Mostrar una etiqueta por cada producto
+        Chart1.Series(0).ChartType = SeriesChartType.Column ' Tipo de gráfico de barras
+    End Sub
+End Class
+
